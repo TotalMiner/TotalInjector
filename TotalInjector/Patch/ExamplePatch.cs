3099fb06673c353747a2eef95fb06b44dc83a588
Justyn <egot@istic.pw>
2018-01-27 18:34:17 -0600

    Moved function to Postfix since thats the purpose

--- TotalInjector/Patch/ExamplePatch.cs
+++ TotalInjector/Patch/ExamplePatch.cs
@@ -14,12 +14,11 @@ namespace TotalInjector.Patch
         public const string TypeName = "StudioForge.TotalMiner.Screens2.ShopMenu";
         public const string MethodName = "TradeCore";
 
-        [HarmonyPrefix]
-        static bool Prefix(object __instance, bool __result, object win, int qty, bool buy)
+        [HarmonyPostfix]
+        static void Postfix(object __instance, bool __result, object win, int qty, bool buy)
         {
             var item = Traverse.Create(win).Property("InvItem").GetValue<InventoryItem>();
             Logger.Info($"Traded {qty} {item.ItemID}");
-            return true;
         }
 
         [HarmonyTargetMethod]
